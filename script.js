94fd98d01a8d23135e16f4805389530bd9b7d2c8
cachimbo28 <felipe.chaves.silva28@escola.pr.gov.br>
2024-12-06 12:13:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-
 avanca.forEach(button => {
-      button.addEventListener('click', function(){
-          const atual = document.querySelector('.ativo');
-          const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+button.addEventListener('click', function(){
+const atual = document.querySelector('.ativo');
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-          atual.classList.remove('ativo');
-          document.getElementById(proximoPasso).classList.add('ativo');
-     })
+atual.classList.remove('ativo');
+document.getElementById(proximoPasso).classList.add('ativo');
+   })
 })
+
+ // Reinicia o jogo ao clicar no botão do reiniício
+ if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+    const atual = document.querySelector('.ativo');
+    atual.classList.remove('ativo');
+    document.getElementById('passo-0').classList.add('ativo');
+    });
+ }
